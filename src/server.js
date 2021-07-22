5291eb09b20eb1c350f5268ad6e57b58d1587a74
Aleksandar Babic <contact@aleksandar-babic.com>
2021-07-22 20:21:33 +0200

    feat(endpoint): Updated the endpoint content (#1)

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) =>
     res.json({
-        content: "Foobar!"
+        content: "Foobar!!"
     }));
 app.listen(3000);
