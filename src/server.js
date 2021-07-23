7a53aef04500e71328c3c9d6834c84cb703437b3
Aleksandar Babic <contact@aleksandar-babic.com>
2021-07-23 16:41:11 +0200

    feat(ping): initial implementation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) =>
+app.get('/ping', (req, res) =>
     res.json({
         content: "Foobar hello!!"
     }));
