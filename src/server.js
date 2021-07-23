4fdce4429356c695ce35b97ec03d364b2450b0bf
Aleksandar Babic <contact@aleksandar-babic.com>
2021-07-23 16:36:16 +0200

    feat(apiendpoint): Added really important hello in the endpoint

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) =>
     res.json({
-        content: "Foobar!!"
+        content: "Foobar hello!!"
     }));
 app.listen(3000);
